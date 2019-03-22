510fb0dae35c34e607757847e2c0ad1257a23481
zhuchao <479346874@qq.com>
2019-03-22 15:34:09 +0800

    fghj

--- app/Http/Controllers/Index/TestController.php
+++ app/Http/Controllers/Index/TestController.php
@@ -9,7 +9,30 @@ use Illuminate\Support\Facades\Redis;
 
 class TestController extends Controller
 {
-    public function login(){
-        echo 111;
+    public function login(Request $request){
+        //echo 111;
+
+            $name=$request->input('u_name');
+            $password=$request->input('u_pwd');
+            $data = [
+                'u_name'    =>  $name,
+                'u_pwd'     =>  $password
+            ];
+            //$url = 'http://passport.lara.com/api/login';
+            $url = 'http://passport.hz4155.cn/api/login';
+            $ch = curl_init($url);
+            curl_setopt($ch,CURLOPT_HEADER,0);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+            curl_setopt($ch,CURLOPT_POST,1);
+            curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $response = json_decode($response,true);
+
+
+            return $response;
+
+
+
+        }
     }
-}
